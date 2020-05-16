2a50afb35e14ea7ec3e4966da1ce4ed9518817ed
SeungYeon <yeondiary@gmail.com>
2020-05-17 00:02:50 +0900

    [6781] 삼삼 트리플 게임

---
+++ 6781.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// 삼삼 트리플 게임
+
+int T;
+
+int main() {
+    
+    cin >> T;
+    
+    for(int test_case = 0; test_case < T; test_case++) {
+        
+        int answer = 0;
+        
+        cout << "#" << test_case + 1 << " " << answer << endl;
+    }
+    
+    
+    return 0;
+}
