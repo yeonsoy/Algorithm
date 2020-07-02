45255c642da4961a3721181ab77c1ea65da28143
SeungYeon <yeondiary@gmail.com>
2020-07-02 18:39:46 +0900

    [10951] A+B - 4

---
+++ 10951.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// A+B - 4
+
+int main()
+{
+    int a, b;
+    
+    while(cin >> a && cin >> b)
+        cout << a + b << endl;
+    
+    return 0;
+}
