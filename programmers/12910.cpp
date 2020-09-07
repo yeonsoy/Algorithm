969396e43b55ecb95a0b99d7dd340957586b4741
SeungYeon <yeondiary@gmail.com>
2020-09-07 10:27:20 +0900

    [12910] 나누어 떨어지는 숫자 배열

---
+++ programmers/12910.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 나누어 떨어지는 숫자 배열
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    for(int i = 0; i<arr.size(); i++)
+    {
+        if(arr[i] % divisor == 0)
+            answer.push_back(arr[i]);
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    if(answer.size() == 0)
+        answer.push_back(-1);
+    
+    return answer;
+}
