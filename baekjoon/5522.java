f4eaa85c6e390cb384912bb16e7ce532bc439f18
SeungYeon <yeondiary@gmail.com>
2020-10-15 13:10:39 +0900

    [5522] 카드게임

---
+++ baekjoon/5522.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 카드게임
+
+public class BOJ5522 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        for(int i = 0; i<5; i++)
+            sum += Integer.parseInt(br.readLine());
+        
+        System.out.print(sum);
+    }
+}
