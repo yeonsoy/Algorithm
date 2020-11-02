36512f4e99f5c2556425b274375f7608ec383e3e
SeungYeon <yeondiary@gmail.com>
2020-11-02 11:26:00 +0900

    [1927] 최소 힙

---
+++ baekjoon/1927.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+// ÃÖ¼Ò Èü
+
+int N;
+priority_queue <int> pq;
+
+int main() {
+
+	fastio();
+
+	cin >> N;
+
+	int temp;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+
+		if (temp == 0) {
+			if (pq.empty()) cout << 0 << "\n";
+			else
+			{
+				cout << -pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+			pq.push(-temp);
+	}
+
+
+	return 0;
+}
