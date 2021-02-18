7b927294e128650b1abea0966b872f2a4ef8a04b
SeungYeon <yeondiary@gmail.com>
2021-02-18 23:24:50 +0900

    [5032] 탄산 음료

---
+++ baekjoon/5032.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// 탄산 음료
+
+int e, f, c;
+
+int main() {
+
+    cin >> e >> f >> c;
+
+    int empty_bottle = e + f;
+    int answer = 0;
+
+    while (true) {
+
+        if (empty_bottle >= c) {
+            answer += (empty_bottle / c);
+            empty_bottle = (empty_bottle % c) + (empty_bottle / c);
+        }
+        else break;
+
+    }
+    cout << answer << endl;
+
+    return 0;
+}
